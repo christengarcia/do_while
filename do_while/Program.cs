b804eb6459197d0296b52ce80cf9c07076be20c0
Christen Garcia <30610163+christengarcia@users.noreply.github.com>
2017-11-23 21:52:21 +1100

    First commit

---
+++ do_while/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace do_while
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int counter = 100;
+
+            do
+            {
+                Console.WriteLine("Counter = {0}", counter);
+                counter++;
+            }
+            while (counter > 2);
+        }
+    }
+}
